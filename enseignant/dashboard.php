a621dc387e80300ef6a919c8dc8770687ac2732f
mohamed_bouth <mohamedbouth87@gmail.com>
2025-12-11 14:24:32 +0100

    QV-5 programing html/css for dashboard page

---
+++ enseignant/dashboard.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../css/dashboard.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo-container">
+            <img src="../img/qodex-logo.png" alt="">
+        </div>
+        <div class="links-container">
+            <div class="links-cont">
+                <img src="../img/dashboard-logo.png" alt="">
+                <a href="">Dashbord</a>
+            </div>
+            <div class="links-cont">
+                <img src="../img/categories-logo.png" alt="">
+                <a href="">Categories</a>
+            </div>
+            <div class="links-cont">
+                <img src="../img/myquiz-logo.png" alt="">
+                <a href="">My Quiz</a>
+            </div>
+            <div class="links-cont">
+                <img src="../img/results-logo.png" alt="">
+                <a href="">Results</a>
+            </div>
+        </div>
+        <div class="switch-container">
+            <div class="switch-container-logo">
+                <p>MK</p>
+            </div>
+            <div>
+                <p>mohamed bouth</p>
+                <p>teacher</p>
+            </div>
+            <div class="switch-button-list">
+                <img src="../img/switch-button-logo.png" alt="">
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="hero-section">
+            <div class="left-hero-section">
+                <h1>Teacher Dashboard</h1>
+                <h3>Manage your quizzes and track your students performance</h3>
+                <div class="btn-container">
+                    <a href="" class="left-btn">
+                        <img src="../img/files-logo-btn.png" alt="">
+                        <p>New Categories</p>
+                    </a>
+                    <a href="" class="right-btn">
+                        <img src="../img/list-logo-btn.png" alt="">
+                        <p>Create a Quiz</p>
+                    </a>
+                </div>
+            </div>
+            <div class="right-hero-section">
+                <img src="../img/generated-image-dashboard.png" alt="">
+            </div>
+        </section>
+        <section class="statistique-section">
+            <div class="statistique-container">
+                <div class="statistique-container-paragraf-cont">
+                    <p class="statistique-title">Total Quiz</p>
+                    <p class="statistique-num">24</p>
+                </div>
+                <div>
+                    <img src="../img/dashboard-statistique-quiz.png" alt="">
+                </div>
+            </div>
+            <div class="statistique-container">
+                <div class="statistique-container-paragraf-cont">
+                    <p class="statistique-title">Categories</p>
+                    <p class="statistique-num">7</p>
+                </div>
+                <div>
+                    <img src="../img/dashboard-statistique-categories.png" alt="">
+                </div>
+            </div>
+            <div class="statistique-container">
+                <div class="statistique-container-paragraf-cont">
+                    <p class="statistique-title">Active Student</p>
+                    <p class="statistique-num">57</p>
+                </div>
+                <div>
+                    <img src="../img/dashboard-statistique-student.png" alt="">
+                </div>
+            </div>
+            <div class="statistique-container">
+                <div class="statistique-container-paragraf-cont">
+                    <p class="statistique-title">Average Note</p>
+                    <p class="statistique-num">24</p>
+                </div>
+                <div>
+                    <img src="../img/dashboard-statistique-note.png" alt="">
+                </div>
+            </div>
+        </section>
+    </main>
+    <script src="../js/dashboard.js"></script>
+</body>
+
+</html>
