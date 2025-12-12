9b2b8d65c1b7db5e1bedefe5ebd4352a750c8145
mohamed bouth <mohamedbouth87@gmail.com>
2025-12-12 13:54:02 +0100

    QV-7 programing html/css for quiz page

--- enseignant/manage_quizzes.php
+++ enseignant/manage_quizzes.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My quiz</title>
     <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/quiz.css">
 </head>
 <body>
     <?php include "../includes/header.php"; ?>
@@ -21,6 +22,37 @@
             </div>
             <div class="right-hero-section">
                 <img src="../img/generated-image-quiz.png" alt="">
+            </div>
+    </section>
+    <section class="quiz-container-section">
+            <div class="quiz-container">
+                <div class="quiz-container-up">
+                    <div class="quiz-container-up-title">
+                        <div>
+                            <p class="type">HTML/CSS</p>
+                        </div>
+                        <p class="title">the basic of HTML5</p>
+                        <p class="des">Test your knowledge of HTML5 elements</p>
+                    </div>
+                    <div class="quiz-container-up-btn">
+                        <img src="../img/edit-icon.png" alt="">
+                        <img src="../img/delete-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="quiz-container-down">
+                    <div class="quiz-container-down-left">
+                        <img src="../img/num-quiz-icon.png" alt="">
+                        <p>12 Quiz</p>
+                    </div>
+                    <div class="quiz-container-down-right">
+                        <img src="../img/num-student-icon.png" alt="">
+                        <p>45 Students</p>
+                    </div>
+                </div>
+                <button class="see-btn">
+                    <img src="../img/see-results-icon.png" alt="">
+                    <p>See the results</p>
+                </button>
             </div>
     </section>
     <?php include "../includes/footer.php"; ?>
