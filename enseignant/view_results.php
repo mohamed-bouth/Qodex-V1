cbc5aa150cc99b21c6b7da7d2e1823bd4bbb34f0
mohamed bouth <mohamedbouth87@gmail.com>
2025-12-12 15:51:25 +0100

    QV-8 programing html/css for results page

--- enseignant/view_results.php
+++ enseignant/view_results.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>results</title>
     <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/results.css">
 </head>
 <body>
     <?php include "../includes/header.php"; ?>
@@ -16,6 +17,35 @@
             <div class="right-hero-section">
                 <img src="../img/generated-image-results.png" alt="">
             </div>
+    </section>
+    <section class="student-results-section">
+        <div class="student-results-container">
+            <div class="student-results-head">
+                <p class="head">Students</p>
+                <p class="head">Quiz</p>
+                <p>Score</p>
+                <p>Date</p>
+                <p>Staut</p>
+            </div>
+            <div class="student-results">
+                <div class="student-results-name head">
+                <p>MB</p>
+                <p>Mohamed Bouth</p>
+            </div>
+            <div class="head">
+                <p>the basic of JavaScipt</p>
+            </div>
+            <div>
+                <p>18/20</p>
+            </div>
+            <div>
+                <p>04 Dec 2025</p>
+            </div>
+            <div>
+                <p class="staut">Successful</p> 
+            </div>
+        </div>
+        
     </section>
     <?php include "../includes/footer.php"; ?>
 </body>
