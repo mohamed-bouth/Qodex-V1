793aa5ec31fcea153c5324ce232984124b7ccbcd
mohamed bouth <mohamedbouth87@gmail.com>
2025-12-13 16:03:44 +0100

    QV-11 programing html/css for login page

---
+++ auth/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <section class="login-container">
+        <div class="login-container-left">
+            <div class="logo-img-container">
+                <img src="../img/qodex-logo.png" alt="">
+            </div>
+            <div class="login-container-content">
+                <h1>Welcome Back</h1>
+                <p>Welcome back to your quiz Platform</p>
+                <div class="input-container">
+                    <input type="text">
+                    <input type="text">
+                    <a href="">forggeten password</a>
+                </div>
+                <div class="login-container-btn">
+                    <a href="../enseignant/dashboard.php" class="login-btn">Login</a>
+                    <a href="./register.php" class="sign-btn">Sign up</a>
+                </div>
+            </div>
+            <div class="logo-img-container"></div>
+        </div>
+        <div class="login-container-right">
+            <img src="../img/lock-img-login-page.png" alt="">
+        </div>
+    </section>
+</body>
+</html>
